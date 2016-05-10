3fa67b8a00a8f8b048a07f72e9e6822bb3cc4702
Byron Lagrone <byronic.entertainment@gmail.com>
2016-05-10 00:43:31 -0500

    First CLI commit
    Visual Studio's integration did not perform as expected.

---
+++ _2016_05_1GAM.py
@@ -0,0 +1,21 @@
+""" 1GAM - a not-yet-multiplayer turn based tactical fantasy game
+   (that should really be multiplayer)
+ Minimum Viable Product is a single map with a single hero, and some monsters.
+   Later: Terrain, additional characters, and all the proverbial spit 'n polish.
+
+ author: Byron
+"""
+
+# IMPORTS
+import pygame
+import random
+import sys
+from avatar import *
+
+# CLASSES
+
+# NOT A HUGE FAN OF THE MAIN FUNCTION, so this code is all inline.
+print "I'm the main game loop."
+
+x = avatar()
+x.getInventory()
---
+++ avatar.py
@@ -0,0 +1,9 @@
+class avatar(object):
+    """In-game character class. Holds character health, positions, items, and skills"""
+    def __init__(self):
+        print "Avatar initialized."
+
+    def getInventory(self):
+        print "Inventory."
+
+
