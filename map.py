7423b4bf580939a767bf262233a142b34142421e
Byron Lagrone <byronic.entertainment@gmail.com>
2016-05-11 09:37:09 -0500

    Map
    Added the Map class; no actual functionality for this class currently.

---
+++ map.py
@@ -0,0 +1,12 @@
+"""
+ Map class.
+ Can load and save maps, keeps track of tiles for the map -- including tile database information
+"""
+
+class Map:
+	# variables
+	db = [] # maps database
+
+	# functions
+	__init__(self):
+		print "Initialized Map."
